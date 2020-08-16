24668ecebbf6cc3570715e7cf30785fbf5a8e10b
Alex Collins <alex_collins@intuit.com>
2020-08-16 10:43:10 -0700

    master

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,45 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
 
 func main() {
-	cobra.Command{
-		Use: "auto-forward"
+	overrides := &clientcmd.ConfigOverrides{}
+	cmd := cobra.Command{
+		Use: "auto-forward",
 		Run: func(cmd *cobra.Command, args []string) {
-
+			loader := clientcmd.NewDefaultClientConfigLoadingRules()
+			loader.DefaultClientConfig = &clientcmd.DefaultClientConfig
+			clientConfig := clientcmd.NewInteractiveDeferredLoadingClientConfig(loader, overrides, os.Stdin)
+			namespace, _, err := clientConfig.Namespace()
+			if err != nil {
+				log.Fatal(err)
+			}
+			restConfig, err := clientConfig.ClientConfig()
+			if err != nil {
+				log.Fatal(err)
+			}
+			clientset, err := kubernetes.NewForConfig(restConfig)
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = clientset.CoreV1().Pods(namespace).List(context.Background(), v1.ListOptions{})
+			if err != nil {
+				log.Fatal(err)
+			}
 		},
+	}
+	clientcmd.BindOverrideFlags(overrides, cmd.PersistentFlags(), clientcmd.RecommendedConfigOverrideFlags(""))
+	err := cmd.Execute()
+	if err != nil {
+		panic(err)
 	}
 }
---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/alexec/kubectl-auto-forward
+
+go 1.13
+
+require (
+	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/spf13/cobra v1.0.0
+	k8s.io/apimachinery v0.18.8
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/utils v0.0.0-20200815180417-3bc9d57fc792 // indirect
+)
+
+replace (
+	k8s.io/api => k8s.io/api v0.18.8
+	k8s.io/apimachinery => k8s.io/apimachinery v0.18.8
+	k8s.io/client-go => k8s.io/client-go v0.18.8
+)
